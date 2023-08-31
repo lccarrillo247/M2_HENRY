33c660db34256cb13fa632a08a11d55338b0f6c6
lccarrillo247 <lccarrillo247@gmail.com>
2023-08-31 06:35:47 -0500

    Exercises React-Hooks

--- 13-React-Hooks/homework/01 - Exercises/src/components/ContactUs/ContactUs.jsx
+++ 13-React-Hooks/homework/01 - Exercises/src/components/ContactUs/ContactUs.jsx
@@ -1,18 +1,47 @@
 import React from "react";
+import {useDispatch} from "react-redux";
+import { enviarForm } from "../../redux/actions/actions";
 
 const ContactUs = () => {
+
+  const [form, setForm] = React.useState({
+    nombre: "",
+    email: "",
+    asunto: "",
+    mensaje: "",
+  });
+
+  const handleInput = (evento) => {
+    setForm({
+      ...form,
+      [evento.target.name]: evento.target.value,
+  })
+  }
+
+  const dispatch = useDispatch()
+
+  const handleSubmit = () => {
+    dispatch(enviarForm(form));
+    // setForm({
+    //   nombre: "",
+    //   email: "",
+    //   asunto: "",
+    //   mensaje: "",
+    // })
+  };
+
   return (
     <div>
       <form className="contactBg">
         <label htmlFor="nombre">Nombre: </label>
-        <input name="nombre" />
+        <input name="nombre" onChange={handleInput} />
         <label htmlFor="email">Email: </label>
-        <input name="email" />
+        <input name="email" onChange={handleInput} />
         <label htmlFor="asunto">Asunto: </label>
-        <input name="asunto" />
+        <input name="asunto" onChange={handleInput} />
         <label htmlFor="mensaje">Mensaje: </label>
-        <input name="mensaje" />
-        <button>Enviar</button>
+        <input name="mensaje" onChange={handleInput} />
+        <button onClick={handleSubmit} type="submit">Enviar</button>
       </form>
     </div>
   );
